3a25342fa59d2ac34715bc5b21633e68b84b4f74
Rishi Prakash <rishiprakash@Rishis-MacBook-Air.local>
2024-08-14 14:31:23 +0100

    Adding Http Proxy as controller in front of frontend and backend

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6826d9a8284927816cf73d2aec0e47c908d6f2ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f68172b641dceecc752728bc1399443c86561ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
