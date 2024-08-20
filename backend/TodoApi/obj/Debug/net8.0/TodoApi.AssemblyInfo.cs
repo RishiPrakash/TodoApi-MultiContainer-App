510cf4a569bdb38f77edc527e7548ca32c6d76b2
Rishi Prakash <rishiprakash@Rishis-MacBook-Air.local>
2024-08-20 22:14:13 +0100

    Adding deployment, service and ingress files [Draft] for K8

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317220ac0849fc04dc39033ba57b3cd7a1a32537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1351ddd61351a031928d8d368c98b0bb17f0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
