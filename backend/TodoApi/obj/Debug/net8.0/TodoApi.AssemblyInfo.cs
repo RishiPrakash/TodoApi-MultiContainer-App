70570c4ecd18e84d337589e9b5b7b0df42ad72c7
Rishi Prakash <rishiprakash@Rishis-MacBook-Air.local>
2025-05-31 17:40:36 +0100

    Adding template files and updating gitignore

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d345840305a67a8c0d260d5fa318ef717af89afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9d55d57f80cbf73f47a7dd3b08747c9a5b3a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
