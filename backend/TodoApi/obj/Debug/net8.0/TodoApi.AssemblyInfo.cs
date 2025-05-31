067b7523f5f7d632246beaa9765417f9bd973e59
Rishi Prakash <rishiprakash@Rishis-MacBook-Air.local>
2025-05-31 17:38:03 +0100

    SPA is able to talk to API and get the response back on public and authenticated endpoint

--- backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9047664f29617c54936aa52e97f0760df20f8d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d345840305a67a8c0d260d5fa318ef717af89afe")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
